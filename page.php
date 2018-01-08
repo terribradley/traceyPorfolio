e41be67096e8e767447e2d39caf8d3281b5a64b8
terribradley <terribradley3@gmail.com>
2018-01-08 15:57:16 -0800

    Add files via upload

--- page.php
+++ page.php
@@ -1,13 +1,17 @@
 <?php get_header(); ?>
 
+<section class="page-title">
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<section class="banner">
+
   <h1> <?php the_title(); ?> </h1>
+
 </section>
-<section class="conten-paget">
+
+<section class="content-page">
   <div class="content-page-title">
-      <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>-----</h1><h1><?php the_title(); ?></h1>
+    <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a>-----<h1><?php the_title(); ?></h1>
   </div>
+  <hr>
   <div class="content-body">
     <p> <?php the_content(); ?> </p>
   </div>
